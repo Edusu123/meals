fa0251b550c8e1c60aad51441fc9ffee3deb3576
eduardopires <eduardo.c.pires@outlook.com>
2022-01-26 08:48:13 -0300

    Implementing categories screen :hammer:

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 
+import '../components/category_item.dart';
+import '../data/dummy_data.dart';
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 1,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
-      children: [],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos cozinhar?'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 1,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: DUMMY_CATEGORIES.map((e) {
+          return CategoryItem(e);
+        }).toList(),
+      ),
     );
   }
 }
