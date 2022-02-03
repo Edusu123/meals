d5d4a87b9000f58ad9084433021e8cc92e71bf8e
eduardopires <eduardo.c.pires@outlook.com>
2022-02-03 08:29:10 -0300

    Named routes 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'screens/categories_screen.dart';
+import 'screens/categories_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,16 +9,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
-      theme: ThemeData(
-          accentColor: Colors.amber,
-          canvasColor: Color.fromRGBO(255, 254, 229, 1),
-          primarySwatch: Colors.pink,
-          fontFamily: 'Raleway',
-          textTheme: ThemeData.light().textTheme.copyWith(
-              headline6:
-                  TextStyle(fontSize: 20, fontFamily: 'RobotoCondensed'))),
-      home: CategoriesScreen(),
-    );
+        title: 'DeliMeals',
+        theme: ThemeData(
+            accentColor: Colors.amber,
+            canvasColor: Color.fromRGBO(255, 254, 229, 1),
+            primarySwatch: Colors.pink,
+            fontFamily: 'Raleway',
+            textTheme: ThemeData.light().textTheme.copyWith(
+                headline6:
+                    TextStyle(fontSize: 20, fontFamily: 'RobotoCondensed'))),
+        home: CategoriesScreen(),
+        routes: {'/categories-meals': (ctx) => CategoriesMealsScreen()});
   }
 }
