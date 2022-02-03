1d17be99bc84e9463671a78763a02937b3c28893
eduardopires <eduardo.c.pires@outlook.com>
2022-02-03 08:36:35 -0300

    Named Routes 2 :zap:

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'screens/categories_screen.dart';
 import 'screens/categories_meals_screen.dart';
+import 'utils/app-routes.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,7 +19,9 @@ class MyApp extends StatelessWidget {
             textTheme: ThemeData.light().textTheme.copyWith(
                 headline6:
                     TextStyle(fontSize: 20, fontFamily: 'RobotoCondensed'))),
-        home: CategoriesScreen(),
-        routes: {'/categories-meals': (ctx) => CategoriesMealsScreen()});
+        routes: {
+          AppRoutes.HOME: (ctx) => CategoriesScreen(),
+          AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen()
+        });
   }
 }
