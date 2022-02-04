762d0915f9cb7d5c164cecf6e85720c3ca03cacc
eduardopires <eduardo.c.pires@outlook.com>
2022-02-04 09:28:18 -0300

    Meal Component #02 :zap:

--- lib/components/meal_item.dart
+++ lib/components/meal_item.dart
@@ -12,21 +12,85 @@ class MealItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-        child: Card(
-            child: Column(children: [
-              Stack(children: [
+      child: Card(
+        child: Column(
+          children: [
+            Stack(
+              children: [
                 ClipRRect(
                     borderRadius: const BorderRadius.only(
                         topLeft: Radius.circular(15),
                         topRight: Radius.circular(15)),
                     child: Image.network(meal.imageUrl,
-                        fit: BoxFit.cover, height: 250, width: double.infinity))
-              ])
-            ]),
-            elevation: 4,
-            margin: const EdgeInsets.all(10),
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15))),
-        onTap: _selectMeal);
+                        fit: BoxFit.cover,
+                        height: 250,
+                        width: double.infinity)),
+                Positioned(
+                  bottom: 20,
+                  child: Container(
+                    color: Colors.black54,
+                    child: Text(
+                      meal.title,
+                      overflow: TextOverflow.fade,
+                      softWrap: true,
+                      style: const TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 5,
+                    ),
+                    width: 300,
+                  ),
+                  right: 10,
+                )
+              ],
+            ),
+            Padding(
+                child: Row(
+                  children: [
+                    Row(
+                      children: [
+                        const Icon(Icons.schedule),
+                        const SizedBox(
+                          width: 3,
+                        ),
+                        Text('${meal.duration} min')
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Icon(Icons.work),
+                        const SizedBox(
+                          width: 3,
+                        ),
+                        Text(meal.complexityText)
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Icon(Icons.attach_money),
+                        const SizedBox(
+                          width: 3,
+                        ),
+                        Text(meal.costText)
+                      ],
+                    ),
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                ),
+                padding: const EdgeInsets.all(20))
+          ],
+        ),
+        elevation: 4,
+        margin: const EdgeInsets.all(10),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+      onTap: _selectMeal,
+    );
   }
 }
