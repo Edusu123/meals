bfab421d929263365c518220dc9ba6a1e5b00e45
Eduardo Conde Pires <eduardo.c.pires@outlook.com>
2022-04-23 19:08:56 -0300

    :construction: filtrando os dados #01

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:meals/screens/settings_screen.dart';
 
-import 'screens/categories_screen.dart';
+import 'screens/tabs_screen.dart';
 import 'screens/categories_meals_screen.dart';
 import 'screens/meal_detail_screen.dart';
-import 'screens/tabs_screen.dart';
+import 'screens/settings_screen.dart';
+
 import 'utils/app-routes.dart';
+import 'models/meal.dart';
+import 'data/dummy_data.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Meal> _availableMeals = DUMMY_MEALS;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
+      title: 'Vamos Cozinhar?',
       theme: ThemeData(
-        accentColor: Colors.amber,
-        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
         primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
         fontFamily: 'Raleway',
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
         textTheme: ThemeData.light().textTheme.copyWith(
               headline6: const TextStyle(
                 fontSize: 20,
@@ -28,7 +37,8 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         AppRoutes.HOME: (ctx) => const TabsScreen(),
-        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) =>
+            CategoriesMealsScreen(_availableMeals),
         AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
         AppRoutes.SETTINGS: (ctx) => const SettingsScreen(),
       },
--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
 
 import '../components/meal_item.dart';
-import '../data/dummy_data.dart';
 import '../models/category.dart';
+import '../models/meal.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
+  final List<Meal> meals;
+
+  const CategoriesMealsScreen(this.meals);
+
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)?.settings.arguments as Category;
 
-    final categoryMeals = DUMMY_MEALS
-        .where((meal) => meal.categories.contains(category.id))
-        .toList();
+    final categoryMeals =
+        meals.where((meal) => meal.categories.contains(category.id)).toList();
 
     return Scaffold(
-        appBar: AppBar(title: Text(category.title)),
-        body: ListView.builder(
-            itemBuilder: (ctx, index) {
-              return MealItem(categoryMeals[index]);
-            },
-            itemCount: categoryMeals.length));
+      appBar: AppBar(title: Text(category.title)),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(categoryMeals[index]);
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
   }
 }
