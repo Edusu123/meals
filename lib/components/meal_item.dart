93535fa6cf461da3dc71b022e9357e47fe4dd9c4
Eduardo Conde Pires <eduardo.c.pires@outlook.com>
2022-04-23 18:11:43 -0300

    :hammer: fechando a tela e passando dados de volta

--- lib/components/meal_item.dart
+++ lib/components/meal_item.dart
@@ -9,7 +9,18 @@ class MealItem extends StatelessWidget {
   MealItem(this.meal);
 
   void _selectMeal(BuildContext context) {
-    Navigator.of(context).pushNamed(AppRoutes.MEAL_DETAIL, arguments: meal);
+    Navigator.of(context)
+        .pushNamed(
+      AppRoutes.MEAL_DETAIL,
+      arguments: meal,
+    )
+        .then((result) {
+      if (result == null) {
+        print('Sem resultado...');
+      } else {
+        print('nome da refeição é $result.');
+      }
+    });
   }
 
   @override
